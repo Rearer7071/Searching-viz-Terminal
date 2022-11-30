e4801d32fb0380d5a4112affd06e17d5b16c7ec6
Priyansh Mathur <80464081+priyanshmathur@users.noreply.github.com>
2022-11-30 06:58:39 +0530

    Update SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -5,9 +5,7 @@ try:
     content = input()
     new = 2
     tabUrl = "https://google.com/search?q="
-    # term = raw_input(takeCommand())
     webbrowser.open(tabUrl + content, new=new)
 except Exception as e:
     print(e)
     print("Sorry i didn't find that")
-    # speak("Sorry i didn't find that")
